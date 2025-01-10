94ac401cf27e5782f352b1ece457ce9d4040da4d
GaaBaptista <gabriel2.baptista@alumni.usp.br>
2025-01-10 14:33:53 +0000

    update html

--- app.py
+++ app.py
@@ -3,32 +3,32 @@ import streamlit as st
 st.markdown("""
 <div id="chatbot">
   <link rel="stylesheet" href="https://www.gstatic.com/dialogflow-console/fast/df-messenger/prod/v1/themes/df-messenger-default.css">
-  <script src="https://www.gstatic.com/dialogflow-console/fast/df-messenger/prod/v1/df-messenger.js"></script>
-  <df-messenger
-    location="us-east1"
-    project-id="corporativo-ia"
-    agent-id="f1c06274-0f5c-4254-a839-1d5161c3202a"
-    language-code="pt-br"
-    max-query-length="-1">
-    <df-messenger-chat-bubble
-      chat-title="BQ_Query_Generator">
-    </df-messenger-chat-bubble>
-  </df-messenger>
-  <style>
-    #chatbot {
-      z-index: 999;
-      position: fixed;
-      --df-messenger-font-color: #000;
-      --df-messenger-font-family: Roboto Mono;
-      --df-messenger-chat-background: #4CAF50;
-      --df-messenger-message-user-background: #097138;
-      --df-messenger-message-bot-background: #C8E6C9;
-      /* New line to center the chat box */
-      left: 50%;
-      transform: translateX(-50%);
-      bottom: 16px;
-    }
-  </style>
+<script src="https://www.gstatic.com/dialogflow-console/fast/df-messenger/prod/v1/df-messenger.js"></script>
+<df-messenger
+  location="us-east1"
+  project-id="corporativo-ia"
+  agent-id="f1c06274-0f5c-4254-a839-1d5161c3202a"
+  language-code="pt-br"
+  max-query-length="-1">
+  <df-messenger-chat
+   chat-title="BQ_Query_Generator">
+  </df-messenger-chat>
+</df-messenger>
+<style>
+  df-messenger {
+    z-index: 999;
+    position: fixed;
+    --df-messenger-font-color: #000;
+    --df-messenger-font-family: Roboto Mono;
+    --df-messenger-chat-background: #C8E6C9;
+    --df-messenger-message-user-background: #097138;
+    --df-messenger-message-bot-background: #EEF7EE;
+    bottom: 0;
+    right: 0;
+    top: 0;
+    width: 350px;
+  }
+</style>
 </div>
 """, unsafe_allow_html=True)
 
